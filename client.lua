96f7de9f3e0fda83a330266d1d2829141fafd7ce
Alejandro Mendoza <34504445+Ren0X1@users.noreply.github.com>
2020-09-22 16:11:43 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,15 +1,15 @@
 local appid = 'APPID'
 local asset = 'ASSETID'
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(2500)
+	while true do
+		Citizen.Wait(2500)
 		local players = GetActivePlayers()
-        local name = GetPlayerName(PlayerId())
+		local name = GetPlayerName(PlayerId())
 		local id = GetPlayerServerId(PlayerId())
 		SetRichPresence("ID: " .. id .. " | " .. name .. " | " .. #players .. "/128")
 		SetDiscordAppId(appid)
 		SetDiscordRichPresenceAsset(asset)
-    end
+	end
 end)
 
 -- Developed by Ren0X --
